e2ef47f4f64869ece797b61fd5cde191b24fa1a0
zyxkad <zyxkad@gmail.com>
2025-02-26 21:04:47 -0700

    add mixin

--- common/src/main/java/com/github/litermc/jadevs/Constants.java
+++ common/src/main/java/com/github/litermc/jadevs/Constants.java
@@ -1,11 +1,10 @@
-package com.example.examplemod;
+package com.github.litermc.jadevs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Constants {
-
-	public static final String MOD_ID = "examplemod";
-	public static final String MOD_NAME = "ExampleMod";
+	public static final String MOD_ID = "jade_vs";
+	public static final String MOD_NAME = "JadeVS";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
---
+++ forge/src/main/java/com/github/litermc/jadevs/JadeVSMod.java
@@ -0,0 +1,9 @@
+package com.github.litermc.jadevs;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class JadeVSMod {
+	public JadeVSMod() {
+	}
+}
