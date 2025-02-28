197796595f5b99b523611761ca4bc69200ed6c20
zyxkad <zyxkad@gmail.com>
2025-02-27 18:41:51 -0700

    update build scripts

--- forge/src/main/java/com/github/litermc/jadevs/mixin/MixinRequestTilePacket$Handler.java
+++ forge/src/main/java/com/github/litermc/jadevs/mixin/MixinRequestTilePacket$Handler.java
@@ -1,11 +1,11 @@
 package com.github.litermc.jadevs.mixin;
 
+import mcp.mobius.waila.network.RequestTilePacket;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.phys.Vec3;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
-import snownee.jade.network.RequestTilePacket;
 
 import java.util.function.Supplier;
 
@@ -20,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 public class MixinRequestTilePacket$Handler {
 	@Inject(
 		method = "lambda$onMessage$0",
-		at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;distSqr(Lnet/minecraft/core/Vec3i;)D"),
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;m_123331_(Lnet/minecraft/core/Vec3i;)D"),
 		locals = LocalCapture.CAPTURE_FAILHARD,
 		cancellable = true
 	)
@@ -33,7 +33,7 @@ public class MixinRequestTilePacket$Handler {
 
 	@Redirect(
 		method = "lambda$onMessage$0",
-		at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;distSqr(Lnet/minecraft/core/Vec3i;)D")
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;m_123331_(Lnet/minecraft/core/Vec3i;)D")
 	)
 	private static double handleRequest$BlockPos$distSqr(BlockPos blockPos, Vec3i playerPos) {
 		return 0;
