92dbb090bbb691e4e3f1074b5c4e3c5ddff0f764
zyxkad <zyxkad@gmail.com>
2025-08-20 06:43:45 -0400

    add more units

--- common/src/main/java/com/github/litermc/jadevs/util/UnitFormatter.java
+++ common/src/main/java/com/github/litermc/jadevs/util/UnitFormatter.java
@@ -5,20 +5,26 @@ import net.minecraft.network.chat.MutableComponent;
 
 public final class UnitFormatter {
 	private static final Component GRAM = Component.translatable("jade_vs.unit.gram");
-	private static final Component KG = Component.translatable("jade_vs.unit.kg");
-	private static final Component TON = Component.translatable("jade_vs.unit.ton");
+	private static final Component KILOGRAM = Component.translatable("jade_vs.unit.kilogram");
+	private static final Component MEGAGRAM = Component.translatable("jade_vs.unit.megagram");
+	private static final Component GIGAGRAM = Component.translatable("jade_vs.unit.gigagram");
+	private static final Component TERAGRAM = Component.translatable("jade_vs.unit.teragram");
+	private static final Component[] UNITS = new Component[]{MEGAGRAM, GIGAGRAM, TERAGRAM};
 
 	private UnitFormatter() {}
 
 	public static final MutableComponent formatWeight(final long grams) {
 		long n = grams;
-		Component unit = KG;
+		Component unit = KILOGRAM;
 		if (n < 1000) {
 			return Component.literal(String.valueOf(n)).append(GRAM);
 		}
-		if (n >= 10_000_000) {
+		for (final Component u : UNITS) {
+			if (n < 10_000_000) {
+				break;
+			}
 			n /= 1000;
-			unit = TON;
+			unit = u;
 		}
 		return Component.literal(String.format("%.2f", n / 1000.0)).append(unit);
 	}
